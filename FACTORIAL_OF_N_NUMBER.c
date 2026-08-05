f38be8ab8a3941f34f17a43bd1c9db0a71c49a59
joydeepsasnal-dot <joydeepsasnal@gmail.com>
2026-08-05 07:36:11 +0530

    Initial Commit

---
+++ FACTORIAL_OF_N_NUMBER.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int Factorial(int n){
+    if (n == 0 || n == 1){
+        return 1;
+    }else{
+        return n * Factorial(n-1);
+    }
+}
+int main(){
+    int num;
+    printf("Enter the number : ");
+    scanf("%d", &num);
+    printf("Factorial of %d is equal to %d", num,Factorial(num));
+    return 0;
+}
